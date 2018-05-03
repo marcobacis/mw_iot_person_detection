0b851d00edc0a22a56e1adc9baca1a36889aeac0
Marco Bacis <marco.bacis@mail.polimi.it>
2018-05-03 22:00:44 +0200

    Added defines to project-conf

--- project-conf.h
+++ project-conf.h
@@ -4,6 +4,23 @@
 /*---------------------------------------------------------------------------*/
 /* Disable PROP_MODE */
 #define CC13XX_CONF_PROP_MODE 0
+
+
+// Project-related defines
+
+//MQTT topic prefix (in addition to the border-router ID)
+#define RPL_PREFIX "people"
+
+//Threshold to identify movement
+#define T 10
+
+//Period to send mqtt messages when connected & not moving (seconds)
+#define K 10
+
+//Time to wait before checking if the person is moving (seconds)
+#define G 4
+
+
 /*---------------------------------------------------------------------------*/
 #endif /* PROJECT_CONF_H_ */
 /*---------------------------------------------------------------------------*/
